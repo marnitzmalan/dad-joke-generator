5b615a5c901db21f5dcba290cee856ac5ea172de
Marnitz Malan <marnitz.malan@payfast.co.za>
2020-07-13 18:32:57 +0200

    tailwindcss added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home bg-gray-200 w-8/12 mx-auto p-20">
+    <h2 class="text-xl m-6">Can I Haz Dad Jokes</h2>  
+    <h3 class="m-6">Joke Text</h3>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Joke</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  max-height: 500px; 
+}
+</style>
